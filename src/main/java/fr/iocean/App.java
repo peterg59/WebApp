3d3db8a625c17d16c3212f6b62ac4296ecf6d979
Pierre GUYARD <pguyard@DIGINAMIC.LOCAL>
2017-12-14 18:06:58 +0100

    Premier commit

---
+++ src/main/java/fr/iocean/App.java
@@ -0,0 +1,12 @@
+package fr.iocean;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("fr.iocean")
+public class App {
+
+}
---
+++ src/main/java/fr/iocean/MyController.java
@@ -0,0 +1,29 @@
+package fr.iocean;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String foo() {
+		return "Hello world";
+	}
+	
+	@RequestMapping(value = "/images/search/{year}/{month}/{day}", method = RequestMethod.GET)
+	public String date(@PathVariable("year") int y, @PathVariable("month") int m, @PathVariable("day") int d, @RequestParam("sort") String name) throws ParseException {
+		String date = d + "/" + m + "/" + y;
+		
+		Date sdf = new SimpleDateFormat("dd/mm/yyyy").parse(date);
+		
+		return "Liste des images du " + sdf.toString() + " triees par " + name;
+	}
+}
